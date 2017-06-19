2077d627d96d2de1a7a45055235177ae9426dfd2
yokoyamarealestatenyc <naoki.cho7@gmail.com>
2017-06-18 23:09:48 -0400

    added image gallery

---
+++ js/photos.js
@@ -0,0 +1,23 @@
+var th = document.getElementById('thumbnails'),
+  box = document.querySelector('.current-image-wrapper');
+var img = document.createElement('img');
+// first image
+img.src = 'img/living_room.jpg';
+// open 1 image
+box.appendChild(img)
+// on click show 
+th.addEventListener('click', function(e) {
+  var x = e.target.nodeName;
+  if(x != 'IMG') {
+  	return;
+  }
+  var t = e.target,
+    new_src = t.parentNode.href;
+  
+  img.className = 'large';
+  img.src = new_src;
+  box.removeChild(img);
+  box.appendChild(img);
+  
+  e.preventDefault();
+}, false);

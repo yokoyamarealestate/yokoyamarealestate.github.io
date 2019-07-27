02788827b9c94b3182b9eb2f680d62de83a1a555
Naoki Yokoyama <naoki.cho7@gmail.com>
2019-07-27 01:44:02 -0400

    added instructions for navigating galleries

--- js/landing.js
+++ js/landing.js
@@ -1,24 +1,8 @@
-// $(document).ready(function(){
-//   // Add smooth scrolling to all links
-//   $("a").on('click', function(event) {
-
-//     // Make sure this.hash has a value before overriding default behavior
-//     if (this.hash !== "") {
-//       // Prevent default anchor click behavior
-//       event.preventDefault();
-
-//       // Store hash
-//       var hash = this.hash;
-
-//       // Using jQuery's animate() method to add smooth page scroll
-//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//       $('html, body').animate({
-//         scrollTop: $(hash).offset().top-50
-//       }, 800, function(){
-   
-//         // Add hash (#) to URL when done scrolling (default click behavior)
-//         window.location.hash = hash;
-//       });
-//     } // End if
-//   });
-// });
+$(document).ready(function(){
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+		// Take the user to a different screen here.
+		document.getElementById('prompt').innerHTML = "After tapping a photo, swipe left/right to navigate through the gallery.";
+	} else {
+		document.getElementById('prompt').innerHTML = "After clicking a photo, use your arrow keys to navigate through the gallery.";
+	}
+});

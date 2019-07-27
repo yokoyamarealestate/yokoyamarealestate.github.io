90e8dc917eb678e6f76c83e5706f979750daf2c1
Naoki Yokoyama <naoki.cho7@gmail.com>
2019-07-27 01:49:07 -0400

    added js test

--- js/landing.js
+++ js/landing.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	console.log(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent));
 	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
 		// Take the user to a different screen here.
 		document.getElementById('prompt').innerHTML = "After tapping a photo, swipe left/right to navigate through the gallery.";
